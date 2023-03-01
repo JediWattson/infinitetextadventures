ecb2915b65b7f41ccf4603db3fdab11ba8096bdd
Robert Daly <JediWattson@users.noreply.github.com>
2023-03-01 02:38:06 -0500

    setting up ssl connection

--- db/postgres/connection.ts
+++ db/postgres/connection.ts
@@ -1,6 +1,21 @@
-import { Pool } from 'pg';
+import fs from 'fs/promises'
+import { Pool, PoolConfig } from 'pg';
 
-const pool = new Pool();
+
+const config: PoolConfig = {};
+
+if (process.env.NODE_ENV === 'production') {
+    if (!process.env.PGSSLCA || !process.env.PGSSLKEY || !process.env.PGSSLCERT)
+        throw new Error('incorrect import paths for the postgres certs');
+    config.ssl = {
+        rejectUnauthorized: false,
+        ca: fs.readFile(process.env.PGSSLCA).toString(),
+        key: fs.readFile(process.env.PGSSLKEY).toString(),
+        cert: fs.readFile(process.env.PGSSLCERT).toString(),
+      }
+}
+
+const pool = new Pool(config);
 
 export default {
     query: (text: string, params: any) => pool.query(text, params),
