e7c8492d97b187e609a7db338d18d0f9b9dce5c4
Robert Daly <JediWattson@users.noreply.github.com>
2023-03-03 19:02:37 -0500

    Revert "hacking this"
    This reverts commit 5fa728674baaf622ef34fd06be819ba5a67539a2.

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -2,9 +2,8 @@ import Link from "next/link";
 import style from "./style.module.css";
 
 type HomeGameType = { type: string; gameId: string; createdAt: string };
-// type HomePropsType = { games: HomeGameType[] };
-// { games }: HomePropsType
-export default function Home() {
+type HomePropsType = { games: HomeGameType[] };
+export default function Home({ games }: HomePropsType) {
   return (
     <div className={style.homeContainer}>
       <h2>
@@ -13,7 +12,7 @@ export default function Home() {
       </h2>
       <div className={style.linkContainer}>
         <h3>Check out some past games!</h3>
-        {/* {games.map(({ gameId, type, createdAt }: HomeGameType, i) => {
+        {games.map(({ gameId, type, createdAt }: HomeGameType, i) => {
           const date = new Date(createdAt);
           return (
             <Link
@@ -24,7 +23,7 @@ export default function Home() {
               {`${date.toLocaleString()} ${type}`}
             </Link>
           );
-        })} */}
+        })}
       </div>
       <p>
         Send me{" "}
