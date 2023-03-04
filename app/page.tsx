2c314fb5dab08a632ca5e3962b7b6cca3e3daa6b
Robert Daly <JediWattson@users.noreply.github.com>
2023-03-03 19:40:20 -0500

    adding revalidate for fetch cache

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Home from "@/components/home";
 
 export default async function Page() {
-  const res = await fetch(`${process.env.NEXTAUTH_URL}/api`);
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api`, { next: { revalidate: 10 } });
   const games = await res.json();
   return <Home games={games} />;
 }
